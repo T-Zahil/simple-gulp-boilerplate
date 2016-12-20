560d734059374c46569868ca68bd715d040d195e
Thomas Sanlis <Thomas.sanlis@gmail.com>
2016-12-20 20:32:44 +0100

    [global] First commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# simple-gulp-boilerplate
-Simple gulp boilerplate with yarn, sass, zepto and so on
+# Simple gulp boilerplate
+
+This repository provides a simple structure for your small and medium projects. It contains many useful additions, as well as the basic codes to use them.
+This project uses sass, and provides the minimal architecture to work in good conditions, with configuration files: mixins, colors, reset css, media queries ...
+Browser sync allows you to automatically update the page when you are working : no need to press F5 or cmd + r anymore !
+Do not hesitate to make pull requests or comment.
+
+# Installation
+
+Clone or download the repository, and do the following : 
+```
+ $ npm install
+```
+
+
+# Usage
+
+Work into the src folder, and do the following to launch gulp :
+```
+$ gulp
+```
+
+
+# Tools
+
+* gulp
+* yarn
+* sass
+* browser-sync
+* autoprefixer
+* imagemin
+* sourcemaps
+* gulp-concat
+* plumber
+* gulp-uglify
+* modernizr
+* zepto
+* font-awesome
---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    sync         = require('browser-sync').create(),
+    autoprefixer = require('gulp-autoprefixer'),
+    imagemin     = require('gulp-imagemin'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    concat       = require('gulp-concat'),
+    plumber      = require('gulp-plumber'),
+    uglify       = require('gulp-uglify'),
+    modernizr    = require('gulp-modernizr');
+
+// Task HTML
+gulp.task('html', () => {
+    gulp.src('src/*.html')
+    .pipe(plumber())
+    .pipe(gulp.dest('dist/'))
+    .pipe(sync.stream());
+});
+
+// Task style
+gulp.task('style', function() {
+   gulp.src([
+        'src/scss/**/*.scss',
+        'node_modules/font-awesome/css/font-awesome.min.css'
+    ])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    
+    .pipe(sass({ outputStyle : 'compressed'} ))
+    .pipe(autoprefixer('last 2 versions'))
+    
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(sync.stream());
+});
+
+// Task Fonts
+gulp.task('fonts', function() {
+    gulp.src([
+            'src/fonts/**',
+            'node_modules/font-awesome/fonts/**'
+        ])
+    .pipe(plumber())
+    .pipe(gulp.dest('dist/fonts/'))
+    .pipe(sync.stream());
+});
+
+// Task scripts
+gulp.task('scripts', function() {
+    return gulp.src([
+        'node_modules/npm-zepto/zepto/dist/zepto.js',
+        'src/**/*.js'
+    ])
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(sync.stream());
+});
+
+// Hot reload 
+gulp.task('hot-reload', function() {
+    sync.init({ server: 'dist/' });
+    gulp.watch('src/*.html', ['html']);
+    gulp.watch('src/img/*', ['images']);
+    gulp.watch('src/js/**/*.js', ['scripts']);
+    gulp.watch('src/fonts/**/*', ['fonts']);
+    gulp.watch('src/scss/**/*.scss', ['style']);
+    gulp.watch('src/*.html').on('change', sync.reload);
+});
+
+// Image compress
+gulp.task('images', function() {
+    gulp.src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img/'));
+});
+
+//Task Modernizr
+gulp.task('modernizr', function() {
+  gulp.src('src/js/*.js')
+    .pipe(modernizr())
+    .pipe(gulp.dest("dist/js"))
+});
+
+
+//Default run all the tasks
+gulp.task('default', ['scripts', 'fonts', 'style', 'images', 'html', 'hot-reload', 'modernizr']);
